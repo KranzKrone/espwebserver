bc594641ea57e87c3ec85154e97b2b86fdc2d848
KranzKrone <dev@st-page.de>
2018-03-20 19:31:04 +0100

    Better Serverstartup

--- espwebserver.ino
+++ espwebserver.ino
@@ -14,8 +14,8 @@
 #define TEST_MODUS true
 
 #define HOSTNAME "esp8266"
-#define WIFI_SSID "FRITZ!Box 7412"
-#define WIFI_PWORD "47548023669706084540"
+#define WIFI_SSID ""
+#define WIFI_PWORD ""
 
 ESP8266WebServer server(80);
 
@@ -26,64 +26,68 @@ ESP8266WebServer server(80);
 
    @return Normalmode - true, AcceesPointMode - false
 */
-bool startWiFi() {
+void startWiFi() {
 
   // Wenn aus dem Speicher die Zugangsdatengeholt werden konnten verbindet sich der ESP mit dem WLAN. Wenn nicht wird ein Accesspoint geöffnet.
 
-
-  if(TEST_MODUS) {
-
-    WiFi.persistent(false);
-    WiFi.mode(WIFI_OFF);
-    WiFi.mode(WIFI_STA);
-    WiFi.hostname(HOSTNAME);
-    MDNS.begin(HOSTNAME);
-    delay(200);
-    WiFi.begin(WIFI_SSID, WIFI_PWORD);
-
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-
-    Serial.println(WiFi.localIP());
-    return true;
-
+  if (TEST_MODUS) {
+    startWiFiSTA();
   } else {
-
-    IPAddress local_IP(192, 168, 0, 1);
-    IPAddress gateway(192, 168, 4, 9);
-    IPAddress subnet(255, 255, 255, 0);
-
-    WiFi.mode(WIFI_AP);
-    WiFi.hostname(HOSTNAME);
-
-    WiFi.softAPConfig(local_IP, gateway, subnet);
-    WiFi.softAP(HOSTNAME, HOSTNAME);
-
-    Serial.print("IP-Adresse: ");
-    Serial.println(WiFi.softAPIP());
-    Serial.printf("MAC address = %s\n", WiFi.softAPmacAddress().c_str());
-    return false;
+    startWiFiAT();
   }
 }
+
+
+bool startWiFiSTA() {
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_OFF);
+  WiFi.mode(WIFI_STA);
+  WiFi.hostname(HOSTNAME);
+  MDNS.begin(HOSTNAME);
+  delay(200);
+  WiFi.begin(WIFI_SSID, WIFI_PWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println(WiFi.localIP());
+  return true;
+}
+
+/**
+   Accespointmodus
+   Hostname: http://192.168.0.1/
+*/
+bool startWiFiAT() {
+  IPAddress local_IP(192, 168, 0, 1);
+  IPAddress gateway(192, 168, 4, 9);
+  IPAddress subnet(255, 255, 255, 0);
+
+  WiFi.mode(WIFI_OFF);
+  WiFi.mode(WIFI_AP);
+  WiFi.hostname(HOSTNAME);
+
+  WiFi.softAPConfig(local_IP, gateway, subnet);
+  WiFi.softAP(HOSTNAME, HOSTNAME);
+
+  Serial.print("IP-Adresse: ");
+  Serial.println(WiFi.softAPIP());
+  Serial.printf("MAC address = %s\n", WiFi.softAPmacAddress().c_str());
+  return true;
+}
+
 
 
 /**
    Programm startet hier.
 */
-
-
 void setup() {
 
   Serial.begin(9600);
   delay(200);
-  Serial.println("Vibrator wird gestartet!");
-
-  bool ws = startWiFi();
-
-  Serial.print("WiFi wurde gestartet - ");
-  Serial.println(((ws) ? "Normal" : "Accespoint"));
+  Serial.println("Webserver wird gestartet!");
 
   // Hier werden die Seiten im Server definiert.
   server.on("/", []() {
