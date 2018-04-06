b64931b722c98fa75f3b2c1cbc1ddc74e20c2953
KranzKrone <dev@st-page.de>
2018-04-06 15:50:36 +0200

    Add EEPROM
    Speichern der WLAN-Zugangsdaten im EEPROM

--- espwebserver.ino
+++ espwebserver.ino
@@ -10,16 +10,21 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
+#include <EEPROM.h>
+
 
 #define HEADER = "<head><title>ESP Webserver</title></head>"
 
 #define TEST_MODUS true
 
 #define HOSTNAME "esp8266"
-#define WIFI_SSID ""
-#define WIFI_PWORD ""
+
+#define MOTOR 4
 
 ESP8266WebServer server(80);
+
+int drehzahl = 0;
+int delayms = 0;
 
 /**
    Das WiFi wird hier gestartet, die Zugangsdaten werden aus dem EEPROM speicher geholt.
@@ -102,23 +107,77 @@ void setup() {
   Serial.begin(9600);
   delay(200);
   Serial.println("Webserver wird gestartet!");
+  EEPROM.begin(512);
 
   startWiFi();
 
   // Hier werden die Seiten im Server definiert.
   server.on("/", []() {
-    server.send(200, "text/html", "Mini Webserver Beispiel<br /><input type=\"range\" name=\"range\" />");
+    Serial.println("Startup");
+    // read eeprom for ssid and pass
+    Serial.println("Reading EEPROM ssid");
+    String esid;
+    for (int i = 0; i < 32; ++i)
+    {
+      esid += char(EEPROM.read(i));
+    }
+    Serial.print("SSID: ");
+    Serial.println(esid);
+    Serial.println("Reading EEPROM pass");
+    String epass = "";
+    for (int i = 32; i < 96; ++i)
+    {
+      epass += char(EEPROM.read(i));
+    }
+    Serial.print("PASS: ");
+    Serial.println(epass);
+    server.send(200, "text/html", "Mini Webserver Beispiel<br />SSID: " + esid + " | PWD: " + epass);
   });
 
   server.on("/settings/", []() {
     String range = server.arg("range");
     Serial.print("Range  ist ");
     Serial.println(range);
-    server.send(200, "text/html", "<h1>Einstellungen</h1><form><input type=\"range\" name=\"range\" value=" + range + " /><input type=\"submit\" value=\"Speichern\" /></form>");
+    drehzahl = range.toInt() * 10;
+    String rms = server.arg("rms");
+    Serial.print("RMS  ist ");
+    Serial.println(rms);
+    delayms = rms.toInt() * 300;
+    server.send(200, "text/html", "<h1>Einstellungen</h1><form>Range:<input type=\"range\" name=\"range\" value=" + range + " />Delay in MSek * 30<input type=\"range\" name=\"rms\" value=" + rms + " /><input type=\"submit\" value=\"Speichern\" /></form>");
   });
 
-  server.on("/settings/", []() {
-    server.send(200, "text/html", "<h1>Einstellungen</h1><form post=\"handleGenericArgs\"><input type=\"range\" name=\"range\" /><input type=\"submit\" value=\"Speichern\" /></form>");
+  server.on("/wlan/", []() {
+    String ssid = server.arg("ssid");
+    String pwd = server.arg("pwd");
+
+    int lssid = ssid.length();
+    int lpwd = pwd.length();
+
+    String errortext = (lssid == 0) ? "Bitte geben Sie SSID und Passwort ein." : "";
+
+    if (lssid > 0) {
+      Serial.println("writing eeprom ssid:");
+      for (int i = 0; i < ssid.length(); ++i)
+      {
+        EEPROM.write(i, ssid[i]);
+        Serial.print("Wrote: ");
+        Serial.println(ssid[i]);
+      }
+
+      Serial.println("writing eeprom pass:");
+      for (int i = 0; i < pwd.length(); ++i)
+      {
+        EEPROM.write(32 + i, pwd[i]);
+        Serial.print("Wrote: ");
+        Serial.println(pwd[i]);
+      }
+      
+      delay(300);
+      EEPROM.commit();
+    }
+
+
+    server.send(200, "text/html", errortext + "<h1>WLAN-Einstellungen</h1><form><input type=\"text\" name=\"ssid\" /><input type=\"text\" name=\"pwd\" /><input type=\"submit\" value=\"Speichern\" /></form>");
   });
 
   server.onNotFound([]() {
@@ -130,4 +189,10 @@ void setup() {
 
 void loop() {
   server.handleClient();
+
+  if (drehzahl > 200) {
+    analogWrite(MOTOR, drehzahl);
+  } else {
+    analogWrite(MOTOR, 0);
+  }
 }
