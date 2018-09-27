d9ca5bde0027bf6929505e66f18015940bc99151
KranzKrone <dev@st-page.de>
2018-09-27 18:16:47 +0200

    Implement WiFiManager
    Implementing main functions in WiFiManager.h and .cpp
    
    Remove stdio.h and stdlib.h from *.ino

--- WiFiManager.h
+++ WiFiManager.h
@@ -1,12 +1,22 @@
-#include <string>
+// #include <stdlib.h>
+#include <WString.h>
+#include <vector>
 
 class WiFiManager
 {
   public:
-  	WiFiManager();
-  	void getCurrentWiFi();
-  	bool connectWiFi();
-  	bool createWiFiAP();
-  	void reconnectWiFi();
-
+  
+    struct WiFiData {
+      char wifissid[64];
+      char wifiissd[64];
+    };
+    
+    int reconnectcounter;
+    String _lokalip;
+    WiFiManager();
+    void getCurrentWiFi();
+    bool connectWiFi(String wifissid, String wifipass, String wifihost);
+    bool createWiFiAP(String ap_name, String ap_pass);
+    void reconnectWiFi();
+    std::vector<WiFiManager::WiFiData> lookUpWiFi();
 };
--- espwebserver.ino
+++ espwebserver.ino
@@ -1,9 +1,9 @@
 /**
-   Sketch für ESP Webserver
-
-   @author Stefan Krone
-   @version 0.0.0
-*/
+ * Sketch for ESP Webserver, usefull for different projects.
+ * 
+ * @author Stefan Krone
+ * @version 0.0.0
+ */
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -11,10 +11,6 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <FS.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <string>
 #include "ConfigManager.h"
 #include "WiFiManager.h"
 #include "DS18B20.h"
@@ -23,87 +19,22 @@
 #define XMLEND "</root>"
 #define MOTOR 4
 
-using namespace std;
-
 #define ONE_WIRE_BUS 5
 
+// Objects for the programm
+ConfigManager conman;
+WiFiManager wman;
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature templib(&oneWire);
-
 DS18B20 sensoren(ONE_WIRE_BUS);
-
 ESP8266WebServer server(80);
 
+// Variablen welche direct in der Loop benutzt werden.
 int drehzahl = 0;
 int delayms = 0;
 String range = "0";
 String rms = "0";
 
-ConfigManager conman;
-// WiFiManager wman;
-
-/**
-   Das WiFi wird hier gestartet, die Zugangsdaten werden aus dem EEPROM speicher geholt.
-   Wenn keine Zugangsdaten im Speicher hinterlegt worden sind oder kein keine Verbindung zu einem WLAN möglich ist,
-   wird ein Accesspoint geöffnet. Nach 20 Versuchen wird ein AccessPoint geöffnet, die Zeit eines Versuches beträgt 500ms.
-
-   @return Normalmode - true, AcceesPointMode - false
-*/
-void startWiFi() {
-  // Wenn aus dem Speicher die Zugangsdatengeholt werden konnten verbindet sich der ESP mit dem WLAN. Wenn nicht wird ein Accesspoint geöffnet.
-  if (!startWiFiSTA()) {
-    startWiFiAT(false);
-  }
-}
-
-
-bool startWiFiSTA() {
-  // WiFi.persistent(false);
-  WiFi.mode(WIFI_OFF);
-  WiFi.mode(WIFI_STA);
-  WiFi.hostname(conman.cfg.wifihost);
-  MDNS.begin(conman.cfg.wifihost);
-  delay(200);
-  WiFi.begin(conman.cfg.wifissid, conman.cfg.wifipass);
-
-  int i = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
-    delay(100);
-    Serial.print(i);
-    if (i > 50) {
-      Serial.println("Die Verbindung mit dem WLAN konnte nicht aufgebaut werden. Es wird ein Accesspoint gestartet.");
-      return false;
-    }
-    i++;
-  }
-
-  Serial.println("");
-  Serial.println(WiFi.localIP());
-  return true;
-}
-
-/**
-   Accespointmodus
-   Hostname: http://192.168.0.1/
-*/
-bool startWiFiAT(bool openAP) {
-  IPAddress local_IP(192, 168, 0, 1);
-  IPAddress gateway(192, 168, 4, 9);
-  IPAddress subnet(255, 255, 255, 0);
-
-  WiFi.mode(WIFI_OFF);
-  WiFi.mode(WIFI_AP);
-  WiFi.hostname(conman.cfg.wifihost);
-
-  WiFi.softAPConfig(local_IP, gateway, subnet);
-  (openAP) ? WiFi.softAP(conman.cfg.wifihost) : WiFi.softAP(conman.cfg.wifihost, conman.cfg.wifihost);
-
-  Serial.print("IP-Adresse: ");
-  Serial.println(WiFi.softAPIP());
-  Serial.printf("MAC address = %s\n", WiFi.softAPmacAddress().c_str());
-  return true;
-}
 
 /**
    Programm startet hier.
@@ -121,7 +52,11 @@ void setup() {
   conman.loadConfig();
   templib.begin();
   
-  startWiFi();
+  // startWiFi();
+
+  bool ci = wman.connectWiFi(conman.cfg.wifissid, conman.cfg.wifipass, conman.cfg.wifihost);
+  Serial.printf("Mit dem Internet verbunden? - %s\n", (ci) ? "Ja" : "Nein");
+  (!ci) ? wman.createWiFiAP("ESPDEV", "") : Serial.println("Accespoint konnte nicht gestartet werden.");
   
   // Statische Dateien wie CSS, JS wird geladen.
   server.serveStatic("/materialize.min.css", SPIFFS, "/materialize.min.css", "max-age=7200");
